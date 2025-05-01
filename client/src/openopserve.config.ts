d4726b988d7ba70726b50c36b291000c54c670e6
buzz-code <hadasa.schechter@gmail.com>
2025-05-01 18:28:05 +0000

    update Openobserve configuration for event application and enhance environment detection

--- client/src/openopserve.config.ts
+++ client/src/openopserve.config.ts
@@ -3,10 +3,10 @@ import packageJson from '../package.json';
 
 const options = {
     clientToken: '32363366626236332d333131632d343665642d616433372d316332366563386566313330',
-    applicationId: 'demo-yoman-online',
+    applicationId: 'event-yoman-online',
     site: 'api.openobserve.ai',
     service: 'frontend',
-    env: location.href.includes('dev') ? 'development' : 'production',
+    env: location.href.includes('dev') || location.href.includes('localhost') ? 'development' : 'production',
     version: packageJson.version,
     organizationIdentifier: 'hadasa_organization_50794_yN95E6myGED9AWO',
     insecureHTTP: false,
