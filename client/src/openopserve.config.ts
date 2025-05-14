95b26a1b2c4247aed2596cc7f61e3c6c0d0f71b5
buzz-code <hadasa.schechter@gmail.com>
2025-05-14 18:20:21 +0000

    fix: correct service name in Openobserve configuration

--- client/src/openopserve.config.ts
+++ client/src/openopserve.config.ts
@@ -5,7 +5,7 @@ const options = {
     clientToken: '32363366626236332d333131632d343665642d616433372d316332366563386566313330',
     applicationId: 'event-yoman-online',
     site: 'api.openobserve.ai',
-    service: 'frontend',
+    service: 'event-frontend',
     env: location.href.includes('dev') || location.href.includes('localhost') ? 'development' : 'production',
     version: packageJson.version,
     organizationIdentifier: 'hadasa_organization_50794_yN95E6myGED9AWO',
